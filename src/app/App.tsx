9422d0063cdf8a6aeef08beeb6d357e964bc8a03
Irlan Freitas <irlan.freitas@hotmail.com>
2026-04-13 15:28:10 -0300

    feat: adicionando analitcs

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,8 +1,14 @@
-import { createBrowserRouter, RouterProvider } from 'react-router'
-import { routes } from './routes'
+import { createBrowserRouter, RouterProvider } from "react-router";
+import { routes } from "./routes";
+import { Analytics } from '@vercel/analytics/next';
 
-const router = createBrowserRouter(routes)
+const router = createBrowserRouter(routes);
 
 export default function App() {
-  return <RouterProvider router={router} />
+  return (
+    <>
+      <RouterProvider router={router} />
+      <Analytics />
+    </>
+  );
 }
